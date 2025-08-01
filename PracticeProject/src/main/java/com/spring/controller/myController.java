442afd8fa1bb75b1926b92a7d35f60d36bc6f4c4
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-02 01:30:20 +0530

    EmpandEmprepo_Added

---
+++ PracticeProject/src/main/java/com/spring/controller/myController.java
@@ -0,0 +1,91 @@
+package com.spring.controller;
+
+import com.spring.entity.Employee;
+import com.spring.repo.EmployeeRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class myController {
+
+    //get emp by id
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Employee> getDataById(@PathVariable("id") int id){
+        List<Employee> employeeList = EmployeeRepo.getAllEmp();
+        for(Employee e : employeeList){
+            if(id == e.getId()){
+                return new ResponseEntity<>(e, HttpStatus.OK);
+            }
+        }
+        Employee e1 = new Employee();
+        return new ResponseEntity<>(e1,HttpStatus.NOT_FOUND);
+    }
+
+    //get emp data by name
+    @GetMapping("/getByName/{name}")
+    public ResponseEntity<Employee> getDatabyname(@PathVariable("name") String name){
+        List<Employee> employeeList = EmployeeRepo.getAllEmp();
+        for(Employee e : employeeList){
+            if(e.getFname().equals(name)){
+                return new ResponseEntity<>(e,HttpStatus.OK);
+            }
+        }
+        Employee e1 = new Employee();
+        return new ResponseEntity<>(e1, HttpStatus.NOT_FOUND);
+    }
+
+    //get data ignoring capital small uppercase lowercase
+    @GetMapping("/getByNameIgnoreCase/{name}")
+    public ResponseEntity<Employee> dataByName(@PathVariable("name") String name){
+        List<Employee> employeeList = EmployeeRepo.getAllEmp();
+        for(Employee e : employeeList){
+            if(e.getFname().equalsIgnoreCase(name)){
+                return new ResponseEntity<>(e,HttpStatus.OK);
+            }
+        }
+        Employee e1 = new Employee();
+        return new ResponseEntity<>(e1, HttpStatus.NOT_FOUND);
+    }
+
+    //get data by city
+    @GetMapping("/getDataByCity/{city}")
+    public ResponseEntity<List<Employee>> getDataByCity(@PathVariable("city") String city){
+        List<Employee> employeeList = EmployeeRepo.getAllEmp();
+        List<Employee> empCityWise = new ArrayList<>();
+        for(Employee e : employeeList){
+            if(e.getCity().equalsIgnoreCase(city)){
+                empCityWise.add(e);
+            }
+        }
+        return new ResponseEntity<>(empCityWise, HttpStatus.OK);
+    }
+
+    //getbycity
+    @GetMapping("/sorted")
+    public ResponseEntity<List<Employee>> getAllemp(@RequestParam(required = false) String city){
+        System.err.println("City value is : "+city);
+        List<Employee> employeeList = EmployeeRepo.getAllEmp();
+        if(city!=null){
+            //sort by city
+            List<Employee> empCityWise = new ArrayList<>();
+            for(Employee e : employeeList){
+                if(e.getCity().equalsIgnoreCase(city)){
+                    empCityWise.add(e);
+                }
+            }
+            return new ResponseEntity<>(empCityWise, HttpStatus.OK);
+        }
+        else {
+            //return all data
+            return new ResponseEntity<>(employeeList, HttpStatus.OK);
+        }
+    }
+}
---
+++ PracticeProject/src/main/java/com/spring/repo/EmployeeRepo.java
@@ -0,0 +1,23 @@
+package com.spring.repo;
+import com.spring.entity.Employee;
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeeRepo {
+    public static List<Employee> getAllEmp(){
+        Employee e = new Employee(101,"shailaja","kshirsagar","pune");
+        Employee e1 = new Employee(102,"kajal","jagtap","pune");
+        Employee e2 = new Employee(103,"om","chavan","solapur");
+        Employee e3 = new Employee(104,"riya","sawwalakhe","amravati");
+        Employee e4 = new Employee(105,"shivani","wange","amravati");
+        Employee e5 = new Employee(106,"raghav","khandelwal","pune");
+        Employee e6 = new Employee(107,"devidas","dhongade","nashik");
+        Employee e7 = new Employee(108,"mrunal","amrutkar","nashik");
+        Employee e8 = new Employee(109,"shravani","madane","pune");
+        Employee e9 = new Employee(110,"srushti","kamlajkar","sambhajinagar");
+
+        List<Employee> emplist = Arrays.asList(e,e1,e2,e3,e4,e5,e6,e7,e8,e9);
+        return emplist;
+
+    }
+}
